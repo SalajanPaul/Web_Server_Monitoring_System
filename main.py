7f2956b79f43dce6bb5032a81c7b25c048bc6d88
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-04-04 14:14:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+def server_status(url):
+    try:
+        response = requests.get(url)
+        if 200 <= response.status_code < 300:
+            return True
+        else:
+            return False
+    except requests.ConnectionError:
+        return False
+
+def server_monitoring(url_server, verification_interval):
+    while True:
+        if server_status(url_server):
+            print(f"Server {url_server} is online.")
+        else:
+            print(f"Server {url_server} is offline.")
+        time.sleep(verification_interval)
+
+
+if __name__ == "__main__":
+    url_server = "https://www.motocicletekawasaki.ro/"
+    verification_interval = 60
+
+    print(f"Monitoring {url_server}...")
+    server_monitoring(url_server, verification_interval)
